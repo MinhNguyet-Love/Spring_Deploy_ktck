9b8375ecf0cea68b48432a6326333c265376665a
Nguyễn Thị Minh Nguyệt <145785457+MinhNguyet-Love@users.noreply.github.com>
2024-12-17 13:05:04 +0700

    17/12 fixx đăng nhập admin users

--- src/main/java/com/example/shopweb/config/SpringSecurity.java
+++ src/main/java/com/example/shopweb/config/SpringSecurity.java
@@ -16,7 +16,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity {
-
     @Autowired
     private UserDetailsService userDetailsService;
 
